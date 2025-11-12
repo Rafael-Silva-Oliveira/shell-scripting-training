81750dcc782475ae102d5d107eff605f3f6b681e
Rafael-Silva-Oliveira <rafael.slo@outlook.com>
2025-11-12 13:05:15 +0100

    fix: update file path in exercise_1.2.sh and add count and text files

--- exercises/instructions/exercise_1.2.sh
+++ exercises/instructions/exercise_1.2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in C:/Users/rafae/OneDrive/Documents/GitHub/ShellScriptingSIB/shell-scripting-training/exercises/data/sample_[0-9][0-9].fasta; do
+for file in ./exercises/sample_[0-9][0-9].fasta; do
 	echo -n "Max sequence ccounts for a single specis $file:"
 
 	grep -o 's:.*;' "$file" | sort | uniq -c | sort -nr | head -1 
